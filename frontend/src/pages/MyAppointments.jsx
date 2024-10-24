19d6060b444257f02354ffe55b9acb3832455eaa
sathwika <gadesathwika2002@gmail>
2024-10-24 14:31:28 +0530

    added gitignore

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -114,9 +114,9 @@ getUserAppointments()
           </div>
           <div>   </div>
           <div className="flex flex-col gap-2 justify-end">
-       {/* {!item.cancelled && item.payment && <button className="sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50">Paid</button>} */}
+       {/* {!item.cancelled && item.payment && <button className="sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50">Paid</button>} 
        {!item.cancelled && !item.isCompleted  && <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>}    
-        {/* !item.cancelled && !item.payment  onClick={()=>appointmentRazorpay(item._id)}  to be added above  */}
+        !item.cancelled && !item.payment  onClick={()=>appointmentRazorpay(item._id)}  to be added above  */}
           {!item.cancelled &&    !item.isCompleted  && <button onClick={()=>cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">Cancel Appointment</button>} 
           {item.cancelled &&  !item.isCompleted  &&  <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500 ">Appointment Cancelled
           </button>}
